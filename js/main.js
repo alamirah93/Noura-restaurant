e851442349d09edad2096d205d603cc45902f4a4
Noura Fahad AlDahmash <nourafaldahmash@gmail.com>
2019-06-30 20:57:15 +0300

    Accesibility fixes

--- js/main.js
+++ js/main.js
@@ -161,10 +161,11 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.setAttribute('aria-label', 'Image of ' + restaurant.name + ' restaurant');
+  image.alt = restaurant.name + " Restaurant";
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
